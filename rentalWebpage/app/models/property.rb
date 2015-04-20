000c7c09643e5ecd72d973343b8b34408731e55f
Steven Schneider <stevenjr18@gmail.com>
2015-04-20 01:12:38 +0000

    Changed success and error messages for adding/editing/updating/deleting property

--- rentalWebpage/app/models/property.rb
+++ rentalWebpage/app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ActiveRecord::Base
   has_many :reviews
-  validates_presence_of :address, :city, :state, :zipcode
-  validates_numericality_of :zipcode
+  validates_presence_of :address, :city, :state, :zipcode, :message => "is required to continue"
+  validates_numericality_of :zipcode, :message => "must be numberic"
 end
