18bbb5f486acf70fbba368bb31917f8700cda8a0
John Neath and Steven Schneider <stevenjr18@gmail.com>
2015-04-27 03:28:25 +0000

    modified code to display star rating. Also now require user to sign in to add review

--- rentalWebpage/app/models/property.rb
+++ rentalWebpage/app/models/property.rb
@@ -1,5 +1,6 @@
 class Property < ActiveRecord::Base
   has_many :reviews
+  ratyrate_rateable 'property'
   validates_presence_of :address, :city, :state, :zipcode, :message => "is required to continue"
   validates_numericality_of :zipcode, :message => "must be numberic"
 end
--- rentalWebpage/app/models/user.rb
+++ rentalWebpage/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    ratyrate_rater
+  
     def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
@@ -8,6 +10,5 @@ class User < ActiveRecord::Base
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
     end
-      ratyrate_rater
   end
 end
