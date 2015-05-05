4391389934ce1be0118ee07e06063a22ccb78a90
Aaron Miller <amiller81@student.cvtc.edu>
2015-05-05 20:52:15 +0000

    Removed OmniAuth test_mode, and if NOTcurrent_user in show.html.erb

--- rentalWebpage/config/initializers/omniauth.rb
+++ rentalWebpage/config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 OmniAuth.config.logger = Rails.logger
-OmniAuth.config.test_mode=true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '580297872072788', '408e71e371aa524dd8d1570766aad685'
