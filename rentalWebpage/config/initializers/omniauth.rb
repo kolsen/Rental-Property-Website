ec4a7d7d6018ecbcc018e8e31fcf310a8f30c88e
Aaron Miller <amiller81@student.cvtc.edu>
2015-05-05 19:58:16 +0000

    Implemented Like button, and sorted home page for recently reviewed.

--- rentalWebpage/config/initializers/omniauth.rb
+++ rentalWebpage/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 OmniAuth.config.logger = Rails.logger
+OmniAuth.config.test_mode=true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '580297872072788', '408e71e371aa524dd8d1570766aad685'
--- rentalWebpage/test/controllers/review_controller_test.rb
+++ rentalWebpage/test/controllers/review_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionController::TestCase
+  setup do
+    @helpful = reviews(:one)
+  end
+  
+  
   test "should get addReview" do
     get :addReview
     assert_response :success
   end
-
+  
+  test "should get helpful" do
+    get :helpful
+    assert_not_nil @helpful
+  end
+  
 end
